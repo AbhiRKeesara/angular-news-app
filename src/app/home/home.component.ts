fe57497b1100aa30e6f7740d631c88e4ef8fedbe
abhinay keesara <abhinayreddy_keesara@student.uml.edu>
2020-01-23 22:01:32 -1000

    added material design for cards

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { delay } from 'rxjs/operators';
+import {MatDialog} from '@angular/material/dialog';
 
 export interface Category{
   value: String;
@@ -16,34 +17,39 @@ export interface Category{
 export class HomeComponent implements OnInit {
 
   categories: Category[]=[
-    {value: 'science', viewValue: 'business'}]
+    {value: 'science', viewValue: 'science'},
+    {value: 'entertainment', viewValue: 'entertainment'},
+    {value: 'general', viewValue: 'general'},
+    {value: 'health', viewValue: 'health'},
+    {value: 'business', viewValue: 'business'},
+    {value: 'sports', viewValue: 'sports'},
+    {value: 'technology', viewValue: 'technology'}
+    ]
 
-
-  public newsSearchForm: FormGroup;
   private API_KEY = "d7ba3d2c4d6840538017893777ed724c";
   showResultsDiv: boolean = false;
   private API_ENDPOINT ;
   data = [];
 
-  constructor(private httpClient: HttpClient, private formBuilder: FormBuilder) { }
+  constructor(private httpClient: HttpClient,public dialog: MatDialog) { }
 
   makeTheRequestAndGetData(formValue){
-   return this.httpClient.get(`https://newsapi.org/v2/top-headlines?category=${formValue.category}&
+   return this.httpClient.get(`https://newsapi.org/v2/top-headlines?category=${formValue}&
    language=en&country=us&sortBy=publishedAt&apiKey=${this.API_KEY}`).pipe(delay(3000)).subscribe((data) => {
       console.log(data['articles']);
       this.data = data['articles'];
+      this.showResultsDiv= false;
     });
    
   }
 
-  searchButtonClicked(){
+  searchButtonClicked(formValue){
     this.showResultsDiv = true;
+    this.makeTheRequestAndGetData(formValue);
   }
 
   ngOnInit() {
-    this.newsSearchForm = this.formBuilder.group({
-      category:['']
-    })
+   
   }
 
 }
